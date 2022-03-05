2525393fe4f1c4f4aa07f2d6b7b8a379cd9868a3
InsertUsernameH3re <spitz.michal@purkynka.cz>
2022-03-05 02:19:59 +0100

    Succesfully connected database and inserted first query

--- connection.php
+++ connection.php
@@ -2,13 +2,17 @@
 include "data.php";
 
 // Create connection
-$connect = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection to the database failed: " . $conn->connect_error);
 }
 
+echo "Connection established";
 
+$query = "INSERT INTO user (nickname, email, score) VALUES ('Michal' , 'spitz.michal@purkynka.cz', '1000')";
+
+$conn->query($query);
 
 ?>
