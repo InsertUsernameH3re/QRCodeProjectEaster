9450671084fa97a216bdee6e3357712cdf39a3b2
InsertUsernameH3re <spitz.michal@purkynka.cz>
2022-03-26 19:09:21 +0100

    ehm

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
 function Cookie_Create(){
     document.cookie = "email=" + document.getElementById("email").value + "@purkynka.cz" + "; expires=" + "Mon, 11 April 2022 23:59:59 CET; Secure";
-}
-
-function SendEmail() {
-    Email.send({
-        Host: "smtp.seznam.cz",
-        Username: "michal.spitz@seznam.cz",
-        Password: "password",
-        To: 'michal.spitz@gmail.com',
-        From: "michal.spitz@seznam.cz",
-        Subject: "This is the subject",
-        Body: "And this is the body"
-    }).then(
-        message => alert(message)
-    );
+    
 }
